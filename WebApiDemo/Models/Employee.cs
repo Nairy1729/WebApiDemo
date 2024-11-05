6c01d409f07cb37157cc72f95ecbabc492fca01c
Narendra <102171116+Nairy1729@users.noreply.github.com>
2024-11-05 16:07:27 +0530

    Add project files.

---
+++ WebApiDemo/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiDemo.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+        public DateTime DOJ { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
